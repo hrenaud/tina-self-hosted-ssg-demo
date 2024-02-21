6b462d33634fba2fe5a90693c39385094b356fad
Renaud Heluin <renaud.heluin@novagaia.fr>
2024-02-21 17:22:42 +0000

    test

--- tina/database.ts
+++ tina/database.ts
@@ -1,30 +1,41 @@
 import { createDatabase, createLocalDatabase } from "@tinacms/datalayer";
-import { MongodbLevel } from "mongodb-level";
+import { RedisLevel } from "upstash-redis-level";
 import { GitHubProvider } from "tinacms-gitprovider-github";
-import dotenv from "dotenv";
 
-dotenv.config();
-
+// Manage this flag in your CI/CD pipeline and make sure it is set to false in production
 const isLocal = process.env.TINA_PUBLIC_IS_LOCAL === "true";
 
-const branch =
-  process.env.GITHUB_BRANCH ||
+const token = process.env.GITHUB_PERSONAL_ACCESS_TOKEN as string;
+const owner = (process.env.GITHUB_OWNER ||
+  process.env.VERCEL_GIT_REPO_OWNER) as string;
+const repo = (process.env.GITHUB_REPO ||
+  process.env.VERCEL_GIT_REPO_SLUG) as string;
+const branch = (process.env.GITHUB_BRANCH ||
   process.env.VERCEL_GIT_COMMIT_REF ||
-  process.env.HEAD ||
-  "main";
+  "main") as string;
+
+if (!branch) {
+  throw new Error(
+    "No branch found. Make sure that you have set the GITHUB_BRANCH or process.env.VERCEL_GIT_COMMIT_REF environment variable.",
+  );
+}
 
 export default isLocal
   ? createLocalDatabase()
   : createDatabase({
       gitProvider: new GitHubProvider({
         branch,
-        owner: process.env.GITHUB_OWNER!,
-        repo: process.env.GITHUB_REPO!,
-        token: process.env.GITHUB_PERSONAL_ACCESS_TOKEN!,
+        owner,
+        repo,
+        token,
       }),
-      databaseAdapter: new MongodbLevel<string, Record<string, unknown>>({
-        collectionName: branch,
-        dbName: "tinacms-self-host",
-        mongoUri: process.env.MONGODB_URI!,
+      databaseAdapter: new RedisLevel<string, Record<string, any>>({
+        redis: {
+          url:
+            (process.env.KV_REST_API_URL as string) || "http://localhost:8079",
+          token: (process.env.KV_REST_API_TOKEN as string) || "example_token",
+        },
+        debug: process.env.DEBUG === "true" || false,
       }),
+      namespace: branch,
     });
